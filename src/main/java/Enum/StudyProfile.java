648ad8292bbd21e9668005142097fd4d1c80e2c0
Linaz <zellik122711@mail.ru>
2024-04-21 20:32:54 +0800

    First commit

---
+++ src/main/java/Enum/StudyProfile.java
@@ -0,0 +1,19 @@
+package Enum;
+
+public enum StudyProfile {
+    PHYSICS("Физика"),
+    MATHEMATICS("Математика"),
+    BIOLOGY("Биология"),
+    GEOGRAPHY("География"),
+    LINGUISTICS("Лингвистика");
+
+    private final String profileName;
+
+    private StudyProfile(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getProfileName() {
+        return this.profileName;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import ModelClass.Student;
+import ModelClass.University;
+import Enum.StudyProfile;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Мушкин Александр Сергеевич");
+        student.setAvgExamScore((float) 4.0);
+        student.setCurrentCourseNumber(4);
+        student.setUniversityId("0010");
+        System.out.println(student);
+
+        University university = new University();
+        university.setId("0010");
+        university.setFullName("Russian Scientific University");
+        university.setShortName("RSU");
+        university.setYearOfFoundation(1906);
+        university.setMainProfile(StudyProfile.BIOLOGY);
+        System.out.println(university);
+    }
+}
